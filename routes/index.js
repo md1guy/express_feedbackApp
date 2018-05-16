f609775c08ecebbe656613fed1d69ed82070e83d
doc Brown <md1guy@live.com>
2018-05-16 21:58:48 +0300

    minor

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' });
+    res.render('index', { title: 'Feedback'});
 });
 
 module.exports = router;
