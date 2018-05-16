2bcc183729f362a1d1c0d81788e416431323ae38
doc Brown <md1guy@live.com>
2018-05-16 20:36:39 +0300

    project structure

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+
+const indexRouter = require('./routes/index');
+
+var port = process.env.PORT || '3000';
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.listen(port, () => console.log('Server is working on port 3000 now.'));
