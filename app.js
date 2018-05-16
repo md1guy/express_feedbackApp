e9e9190981e0191c6d4b19eb86c4a2c080e40b9d
doc Brown <md1guy@live.com>
2018-05-16 22:49:47 +0300

    cosmetic

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
