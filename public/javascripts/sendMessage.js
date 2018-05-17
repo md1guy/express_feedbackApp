9bb9070a6f40291f9cdf2e5c5abc42a38b61e0b3
doc Brown <md1guy@live.com>
2018-05-17 12:58:39 +0300

    alerts from sweetalert & e-mail validity checking

--- public/javascripts/sendMessage.js
+++ public/javascripts/sendMessage.js
@@ -1,18 +1,69 @@
 function sendMessage() {
 
+    if (document.getElementById('message_field').value.length > 2000) {
+
+        swal("Oops!", "Your message can't be longer than 2000 symbols.", "error");
+
+        return false;
+    }
+
     let xhr = new XMLHttpRequest();
 
-    xhr.open('POST', '/sendFeedback', true);
+    try {
+        xhr.open('POST', '/sendFeedback', true);
 
-    xhr.send(JSON.stringify({
-        name: document.getElementById('name_field').value,
-        email: document.getElementById('email_field').value,
-        message: document.getElementById('message_field').value
-    }));
+        let name = document.getElementById('name_field').value;
+        let email = document.getElementById('email_field').value;
+        let message = document.getElementById('message_field').value;
 
-    document.getElementById('name_field').value = '';
-    document.getElementById('email_field').value = '';
-    document.getElementById('message_field').value = '';
+        if(name.length == 0) {
 
-    return false;
+            swal('Oops!', 'Please, enter your name.', 'error');
+
+            return false;
+        }
+
+        if(!validateEmail(email) && email != '') {
+
+            swal('Oops!', 'Invalid e-mail format. You may leave this field empty.', 'error');
+
+            return false;
+        }
+
+        if (message.length > 2000) {
+
+            swal("Oops!", "Your message can't be longer than 2000 symbols.", "error");
+    
+            return false;
+        } 
+        
+        if (message.length == 0) {
+
+            swal("Oops!", "Please, enter your message.", "error");
+    
+            return false;
+        }
+
+        xhr.send(JSON.stringify({
+            name: name,
+            email: email,
+            message: message
+        }));
+
+        document.getElementById('name_field').value = '';
+        document.getElementById('email_field').value = '';
+        document.getElementById('message_field').value = '';
+
+        swal('Done!', 'Succesfully sent message.', 'success');
+
+        return false;
+    } catch(e) {
+
+        swal('Oops!', 'Something went wrong.', 'error');
+    }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
